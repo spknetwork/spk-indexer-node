8a9f1999d41648056079590aa56e42f784680519
vaultec <47548474+vaultec81@users.noreply.github.com>
2021-10-04 19:07:50 -0700

    feat: P2P data exchange, refactor database schemas
    Using bloom filters for improving syncing effiency.

--- src/modules/graph-indexer/graph-indexer.model.ts
+++ src/modules/graph-indexer/graph-indexer.model.ts
@@ -1,5 +1,5 @@
-import {ObjectId} from 'bson';
-import {SpkCollectionItem} from '../mongo-access/spk-collection-item.type';
+import { ObjectId } from 'bson'
+import { SpkCollectionItem } from '../mongo-access/mongo-access.model'
 /**
  * NOTE about pulling doc authors
  * 
@@ -14,44 +14,51 @@ import {SpkCollectionItem} from '../mongo-access/spk-collection-item.type';
  */
 export class IndexedDocument implements SpkCollectionItem {
   // Internal / mongo only ID
-  _id: ObjectId;
+  _id: ObjectId
   /**
    * Stream ID
    */
-  id: string;
+  id: string
   // Parent StreamID
-  parentId?: string;
-  content: unknown;
-  expire?: any;
-  schemaStreamId?: string;
-  updated_at: Date;
-  last_checked: Date;
+  parent_id?: string
+  content: unknown
+  expire?: any
+  schemaStreamId?: string
+  first_seen: Date
+  last_updated: Date // Note this is our internal calculated value, this may or may not actually be when it was updated in the Ceramic document. TODO: Use Ceramic timestamps
+  last_pinged: Date //Last time the document was checked
+  last_accessed?: Date //Maybe a bit too IO intensive?
+  versionId?: string
+  pinned: boolean
 }
 
 /**
  * A node in the index
  */
 export class IndexedNode implements SpkCollectionItem {
-  _id: ObjectId;
+  _id: ObjectId
   // Stream ID
-  id: string;
-  children: string[];
-  expiration: any;
-  custodian_nodes: any[]; // TODO - figure out the correct type for a custodian node
+  id: string
+  parent_id?: string
+  expiration: any
+  first_seen: Date
+  last_pinged: Date //Last time the subgraph was received.
+  last_pulled?: Date
+  validated?: boolean
 }
 /**
  * A CS entry
  */
 export class CSNode implements SpkCollectionItem {
-  _id: ObjectId;
+  _id: ObjectId
   // Stream ID
-  id: string;
-  custodian_id: string; //Libp2p PeerId
-  last_seen: Date; //Latest date the node has announced it's responsibility of this subgraph
-  first_seen: Date; //Date the node has first announced it's responsibility of this subgraph
-  last_ping: Date; //Date the node has been known to be online
-  ttl?: number; //Period of time the record is valid for and must be rechecked. (ttl + last_seen) = recheck time
-  ttl_time?: Date; //Exact date of recheck
+  id: string
+  custodian_id: string //Libp2p PeerId
+  last_seen: Date //Latest date the node has announced it's responsibility of this subgraph
+  first_seen: Date //Date the node has first announced it's responsibility of this subgraph
+  last_ping: Date //Date the node has been known to be online
+  ttl?: number //Period of time the record is valid for and must be rechecked. (ttl + last_seen) = recheck time
+  ttl_time?: Date //Exact date of recheck
 
   /**
    * Metric used to determine how reputable a peer is;
@@ -60,5 +67,12 @@ export class CSNode implements SpkCollectionItem {
    * Later versions of the software will use more advanced trust metrics to determine the most optimal nodes to use.
    * Initially: the goal here is to prevent a node from joining, then leaving 10 minutes later resulting in network confusion and/or spam in the database.
    */
-  trust_score?: number;
+  trust_score?: number
+}
+
+export interface CeramicContainer {
+  parentId: string
+  content: any
+  namespace: string
+  app_namespace: string
 }
--- src/modules/graph-indexer/services/custodian.service.ts
+++ src/modules/graph-indexer/services/custodian.service.ts
@@ -1,64 +1,49 @@
-import {StreamID} from '@ceramicnetwork/streamid';
-import IPFSHTTP, {IPFSHTTPClient} from 'ipfs-http-client';
-import {Collection} from 'mongodb';
-import NodeSchedule from 'node-schedule';
+import { StreamID } from '@ceramicnetwork/streamid'
+import IPFSHTTP, { IPFSHTTPClient } from 'ipfs-http-client'
+import { Collection } from 'mongodb'
+import NodeSchedule from 'node-schedule'
+import Path from 'path'
+import { BloomFilter } from 'bloom-filters'
 
-import {decode, encode} from '../frame-codec.utils';
-import {CSNode, IndexedNode} from '../graph-indexer.model';
+import { decode, encode } from '../frame-codec.utils'
+import { CSNode, IndexedNode } from '../graph-indexer.model'
+import { SUBChannels, messageTypes } from '../../peer-to-peer/p2p.model'
+import { CoreService } from './core.service'
 
-const IPFS_PUBSUB_TOPIC = '/spk.network/testnet-dev';
+const IPFS_PUBSUB_TOPIC = '/spk.network/testnet-dev'
 
-const CUS_ANNOUNCE = 'cust_announce';
+const CUS_ANNOUNCE = 'cust_announce'
 
 export class CustodianService {
-  ipfs: IPFSHTTPClient;
-  myPeerId: string;
-  graphIndex: Collection<IndexedNode>;
-  graphCs: Collection<CSNode>;
-  constructor(private readonly self) {
-    this.self = self;
+  self: CoreService
+  ipfs: IPFSHTTPClient
+  myPeerId: string
+  graphIndex: Collection<IndexedNode>
+  graphCs: Collection<CSNode>
+  asks: Set<string>
+  constructor(self) {
+    this.self = self
 
-    this.handleSub = this.handleSub.bind(this);
+    this.handleSub = this.handleSub.bind(this)
+    this.announceCustodian = this.announceCustodian.bind(this)
+    this.receiveBloomAnnounce = this.receiveBloomAnnounce.bind(this)
+    this.announceBloom = this.announceBloom.bind(this)
+
+    this.asks = new Set()
   }
   async receiveAnnounce(payload, fromId) {
-    console.log(payload, fromId);
+    console.log(payload, fromId)
     if (this.myPeerId === fromId) {
-      console.warn('peerId is the same as local node');
+      console.warn('peerId is the same as local node')
     }
-    const announce_list = payload.content.sg.map(e =>
-      StreamID.fromBytes(Buffer.from(e, 'base64'))
-    );
+    const announce_list = payload.content.sg.map((e) =>
+      StreamID.fromBytes(Buffer.from(e, 'base64')),
+    )
     for (const streamId of announce_list) {
-      /*console.log(streamId.toString());
-      const data = await this.graphIndex.findOne({
-        id: streamId.toString(),
-      });
-      console.log(data);
-      if (!data) {
-        await this.graphIndex.insertOne({
-          id: streamId.toString(),
-          custodian_nodes: [fromId],
-          expiration: new Date(new Date().getTime() / 1 + 1000000),
-          children: [],
-        });
-      } else {
-        if (!data.custodian_nodes.includes(fromId)) {
-          await this.graphIndex.findOneAndUpdate(
-            {
-              id: streamId.toString(),
-            },
-            {
-              $push: {
-                custodian_nodes: fromId,
-              },
-            }
-          );
-        }
-      }*/
       const csRecord = await this.graphCs.findOne({
         id: streamId.toString(),
         custodian_id: fromId,
-      });
+      })
       if (!csRecord) {
         await this.graphCs.insertOne({
           id: streamId.toString(),
@@ -67,7 +52,7 @@ export class CustodianService {
           last_seen: new Date(),
           last_ping: new Date(),
           ttl: 21600, //6 hours, hardcoding for now. In the future custodian nodes will announce a ttl between 300 seconds and 30 days.
-        });
+        })
         //Note: Custodian records that expire will be removed from the database.
       } else {
         await this.graphCs.findOneAndUpdate(
@@ -79,61 +64,105 @@ export class CustodianService {
               last_seen: new Date(),
               last_ping: new Date(),
             },
-          }
-        );
+          },
+        )
       }
     }
   }
-  announceCustodian = async () => {
-    console.log('running custodian announce');
+  async receiveBloomAnnounce(payload, fromId) {
+    console.log('receiving bloom announce')
+    console.log(payload, fromId)
+    const bloomFilter = BloomFilter.fromJSON(JSON.parse(payload.bloom))
+    console.log(bloomFilter)
+    const arrayItems = await this.graphIndex
+      .find({
+        parent_id: payload.parent_id,
+      })
+      .toArray()
+    const output = []
+    for (const item of arrayItems) {
+      if (!bloomFilter.has(item.id)) {
+        console.log('has')
+        output.push(item.id)
+      }
+    }
+    const msg = {
+      type: messageTypes.CUS_RES_SUBGRAPH,
+      content: {
+        sg: output,
+      },
+    }
+    console.log(msg)
+    void this.ipfs.pubsub.publish(
+      Path.posix.join(IPFS_PUBSUB_TOPIC, SUBChannels.CUSTODIAN_SYNC),
+      encode(msg),
+    )
+  }
+  async announceCustodian() {
     const out = (
       await this.self.graphDocs
         .find({
           parent_id: null,
-          streamId: {$exists: true},
+          streamId: { $exists: true },
         })
         .toArray()
-    ).map(e =>
-      Buffer.from(StreamID.fromString(e.streamId).bytes).toString('base64')
-    );
+    ).map((e) => Buffer.from(StreamID.fromString(e.id).bytes).toString('base64'))
     const msg = {
       type: CUS_ANNOUNCE,
       content: {
         sg: out,
       },
-    };
-    console.log(JSON.stringify(msg));
-    const codedMessage = encode(msg);
-    console.log(codedMessage);
-    await this.ipfs.pubsub.publish(IPFS_PUBSUB_TOPIC, codedMessage);
-  };
-  handleSub = message => {
-    console.log('ln 44 pass check');
-    let msgObj;
+    }
+    const codedMessage = encode(msg)
+    await this.ipfs.pubsub.publish(IPFS_PUBSUB_TOPIC, codedMessage)
+  }
+  async announceBloom() {
+    console.log('announcing bloom')
+    const parent_id = 'kjzl6cwe1jw14b57249n2ujjkiiucpdw9dic9rotvk2m1tlfbmoeo7ccwkz94ho'
+    const bloomFilter = await this.self.createBloom(parent_id)
+    const msg = {
+      type: messageTypes.CUS_ASK_SUBGRAPH,
+      parent_id,
+      bloom: JSON.stringify(bloomFilter.saveAsJSON()),
+    }
+    console.log(Path.posix.join(IPFS_PUBSUB_TOPIC, SUBChannels.CUSTODIAN_SYNC))
+    const codedMessage = encode(msg)
+    await this.ipfs.pubsub.publish(
+      Path.posix.join(IPFS_PUBSUB_TOPIC, SUBChannels.CUSTODIAN_SYNC),
+      codedMessage,
+    )
+  }
+  handleSub(message) {
+    let msgObj
     try {
-      msgObj = decode(message.data);
+      msgObj = decode(message.data)
     } catch (ex) {
-      console.log(ex);
-      return;
+      return
     }
-    console.log(message);
-    console.log('receiving announce 12');
+    console.log('receiving RDM' + msgObj.type)
     if (msgObj.type === CUS_ANNOUNCE) {
-      console.log('receiving announce 52');
-      void this.receiveAnnounce(msgObj, message.from);
+      void this.receiveAnnounce(msgObj, message.from)
+    } else if (msgObj.type === messageTypes.CUS_ASK_SUBGRAPH) {
+      void this.receiveBloomAnnounce(msgObj, message.from)
     }
-  };
+  }
+  async *queryChildren(id: string) {}
   async start() {
-    this.graphIndex = this.self.db.collection('graph.index');
-    this.graphCs = this.self.db.collection('graph.cs');
-    this.ipfs = IPFSHTTP.create();
+    this.graphIndex = this.self.db.collection('graph.index')
+    this.graphCs = this.self.db.collection('graph.cs')
+    this.ipfs = IPFSHTTP.create()
 
-    this.myPeerId = (await this.ipfs.id()).id;
-    void this.ipfs.pubsub.subscribe(IPFS_PUBSUB_TOPIC, this.handleSub);
-    NodeSchedule.scheduleJob('* * * * *', this.announceCustodian);
+    this.myPeerId = (await this.ipfs.id()).id
+    void this.ipfs.pubsub.subscribe(IPFS_PUBSUB_TOPIC, this.handleSub)
+    void this.ipfs.pubsub.subscribe(
+      Path.posix.join(IPFS_PUBSUB_TOPIC, SUBChannels.CUSTODIAN_SYNC),
+      this.handleSub,
+    )
+    NodeSchedule.scheduleJob('* * * * *', this.announceCustodian)
+    NodeSchedule.scheduleJob('* * * * *', this.announceBloom)
   }
   async stop() {
-    console.log('system is stopping');
-    void this.ipfs.pubsub.unsubscribe(IPFS_PUBSUB_TOPIC, this.handleSub);
+    console.log('system is stopping')
+    void this.ipfs.pubsub.unsubscribe(IPFS_PUBSUB_TOPIC, this.handleSub)
   }
 }
